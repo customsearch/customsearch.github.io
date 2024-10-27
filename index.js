6808a6a583def27c31d8e72ed30771200dfc1ac8
danqdinh <116571017+danqdinh@users.noreply.github.com>
2024-10-27 18:00:56 +0700

    Added Italian Verbs button

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ function camdict_search(){
     window.location = `https://dictionary.cambridge.org/dictionary/english/${Input}`;
 }
 
+// Italian Verbs
+function itaverbs_search(){
+    var Input = d$("#Input").value.trim();
+    window.location = `https://www.italian-verbs.com/italian-verbs/conjugation.php?parola=${Input}`;
+}
+
 // Word Ref
 function wordref_search(){
     var Input = d$("#Input").value.trim();
@@ -55,14 +61,16 @@ function main(){
     var Input          = location.search.replace("?q=","");
     d$("#Input").value = decodeURIComponent(Input);
 
-    d$("#Copilot-Btn").addEventListener("click", copilot_ask);
-    d$("#Google-Btn") .addEventListener("click", google_search);
-    d$("#Camdict-Btn").addEventListener("click", camdict_search);
-    d$("#Wordref-Btn").addEventListener("click", wordref_search);
+    d$("#Copilot-Btn") .addEventListener("click", copilot_ask);
+    d$("#Google-Btn")  .addEventListener("click", google_search);
+
+    d$("#Camdict-Btn") .addEventListener("click", camdict_search);
+    d$("#Itaverbs-Btn").addEventListener("click", itaverbs_search);
+    d$("#Wordref-Btn") .addEventListener("click", wordref_search);    
     
-    d$("#Gtrans-Btn") .addEventListener("click", gtrans_search);
-    d$("#Hvdic-Btn")  .addEventListener("click", hvdic_search);
-    d$("#Wiktion-Btn").addEventListener("click", wiktion_search);
+    d$("#Gtrans-Btn")  .addEventListener("click", gtrans_search);
+    d$("#Hvdic-Btn")   .addEventListener("click", hvdic_search);
+    d$("#Wiktion-Btn") .addEventListener("click", wiktion_search);
 }
 
 window.onload = main;
