209a0c948cf4cf6a85d2b55042c5907fe8394943
danqdinh <116571017+danqdinh@users.noreply.github.com>
2024-10-31 14:18:27 +0700

    Removed cn

--- index.js
+++ index.js
@@ -38,24 +38,6 @@ function wordref_search(){
     window.location = `https://www.wordreference.com/iten/${Input}`;
 }
 
-// G Trans
-function gtrans_search(){
-    var Input = d$("#Input").value.trim();
-    window.location = `https://translate.google.com/?sl=auto&tl=en&op=translate&text=${Input}`;
-}
-
-// HV Dic
-function hvdic_search(){
-    var Input = d$("#Input").value.trim();
-    window.location = `https://hvdic.thivien.net/whv/${Input}`;
-}
-
-// Wiktionary
-function wiktion_search(){
-    var Input = d$("#Input").value.trim();
-    window.location = `https://en.wiktionary.org/wiki/${Input}`;
-}
-
 // Main
 function main(){
     var Input          = location.search.replace("?q=","");
@@ -66,11 +48,7 @@ function main(){
 
     d$("#Camdict-Btn") .addEventListener("click", camdict_search);
     d$("#Itaverbs-Btn").addEventListener("click", itaverbs_search);
-    d$("#Wordref-Btn") .addEventListener("click", wordref_search);    
-    
-    d$("#Gtrans-Btn")  .addEventListener("click", gtrans_search);
-    d$("#Hvdic-Btn")   .addEventListener("click", hvdic_search);
-    d$("#Wiktion-Btn") .addEventListener("click", wiktion_search);
+    d$("#Wordref-Btn") .addEventListener("click", wordref_search);
 }
 
 window.onload = main;
